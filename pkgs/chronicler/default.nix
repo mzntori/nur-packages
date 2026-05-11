c0ef257f7b110e98cf35c5fbc956328dc8fe1a03
mzntori <mzntori@proton.me>
2026-05-11 21:51:30 +0200

    chronicler: add metadata

--- pkgs/chronicler/default.nix
+++ pkgs/chronicler/default.nix
@@ -7,18 +7,34 @@
   fixWebkit ? false,
   fixWayland ? false,
 }:
+
 let
+  version = "0.51.4";
+
   chronicler-unwrapped = appimageTools.wrapType2 {
     pname = "chronicler";
-    version = "0.51.4-alpha";
+    version = "${version}-alpha";
     src = fetchurl {
-      url = "https://github.com/mak-kirkland/chronicler/releases/download/v0.51.4-alpha/Chronicler_0.51.4_amd64.AppImage";
+      url = "https://github.com/mak-kirkland/chronicler/releases/download/v${version}-alpha/Chronicler_${version}_amd64.AppImage";
       sha256 = "sha256-/hPVY954StH95cUyGj3su/VaXYLpsjjCpB8uu5ssh8g=";
     };
   };
+
+  wrapper-script = ''
+    export WEBKIT_DISABLE_DMABUF_RENDERER=${if fixWebkit then "1" else ""}
+    export LD_PRELOAD=${if fixWayland then wayland + "/lib/libwayland-client.so" else ""}
+    exec ${chronicler-unwrapped}/bin/chronicler
+  '';
 in
-writeShellScriptBin "chronicler" ''
-  export WEBKIT_DISABLE_DMABUF_RENDERER=${if fixWebkit then "1" else ""}
-  export LD_PRELOAD=${if fixWayland then wayland + "/lib/libwayland-client.so" else ""}
-  exec ${chronicler-unwrapped}/bin/chronicler
-''
+
+writeShellScriptBin "chronicler" wrapper-script
+// {
+  meta = {
+    description = "a free, offline worldbuilding tool built for writers, novelists, and tabletop RPG game masters";
+    homepage = "https://chronicler.pro/";
+    license = {
+      fullName = "PolyForm Shield License 1.0.0";
+      url = "https://polyformproject.org";
+    };
+  };
+}
